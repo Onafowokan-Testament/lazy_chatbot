69524ed54606c903eea418843b023669dc915dfe
Onafowokan-Testament <onafowokan.testament@stu.cu.edu.ng>
2025-02-11 13:56:18 +0100

    DEFINE enviroment varaible

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import sequential
-from langchain_core.outPut_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser
 
 from dotenv import load_dotenv
 import os
 import streamlit as st
+load_dotenv()
+
+os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
+os.environ['LANGCHAIN_TRACING_V2'] = 'true'
+os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
+
+
+
 
